56e0b844bb8ffd6800b8c0f48b6ffc34809db704
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-06-18 20:48:42 +0000

    Added snack sprite to middle of scene

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,24 @@
 
 #include "game.h"
 
+#include "bn_sprite_items_snack.h"
+
+#include "bn_sprite_animate_actions.h"
+
 
 int main() {
     bn::core::init();
     game g = game();
+
+    bn::sprite_ptr goldfish = bn::sprite_items::snack.create_sprite();
+
+    bn::sprite_animate_action<4> anim = bn::create_sprite_animate_action_forever(goldfish, 9, 
+        bn::sprite_items::snack.tiles_item(), 0, 1, 2, 3);
+
     
     while(true) {
         g.update();
+        anim.update();
         bn::core::update();
     }
 }
