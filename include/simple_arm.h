ce1cdf81c824cbe9781e02ed6962d1ed9b8aaf5c
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-09-11 22:56:37 +0000

    Beginning of multijoint arm

--- include/simple_arm.h
+++ include/simple_arm.h
@@ -8,28 +8,30 @@ static constexpr int SIMPLE_ARM_LENGTH = 64;
 
 class fish_game;
 
-enum class arm_state {
+enum class arm_state
+{
     MOVING,
     SWIPING,
     RETURNING
 };
 
-class simple_arm {
-    public:
-        simple_arm(fish_game& g, bn::rect bounds, bn::fixed move_speed=1, bn::fixed swipe_speed=10);
-        void update();
-        void set_state(arm_state state);
-        bn::optional<bn::rect> hitbox(); // Gives the active hitbox if it exists. Hitbox is only active when swiping
-    private:
-        fish_game& _g;
-        bn::sprite_ptr _spr;
-        bn::rect _bounds;
-        bn::fixed _move_speed;
-        bn::fixed _swipe_speed;
-        arm_state _state;
-        
-        void _move();
-        void _swipe();
-        void _return();
+class simple_arm
+{
+public:
+    simple_arm(fish_game &g, bn::rect bounds, bn::fixed move_speed = 1, bn::fixed swipe_speed = 10);
+    void update();
+    void set_state(arm_state state);
+    bn::optional<bn::rect> hitbox(); // Gives the active hitbox if it exists. Hitbox is only active when swiping
+private:
+    fish_game &_g;
+    bn::sprite_ptr _elbow_spr;
+    bn::sprite_ptr _paw_spr;
+    bn::rect _bounds;
+    bn::fixed _move_speed;
+    bn::fixed _swipe_speed;
+    arm_state _state;
 
+    void _move();
+    void _swipe();
+    void _return();
 };
--- src/simple_arm.cpp
+++ src/simple_arm.cpp
@@ -7,7 +7,6 @@
 #include "fish.h"
 #include "fish_game.h"
 
-
 #include <bn_keypad.h>
 
 #include "bn_sprite_items_fritozoomarm.h"
@@ -19,22 +18,29 @@ static constexpr int HITBOX_WIDTH = 8;
 
 static constexpr int PAW_LOC = (SIMPLE_ARM_LENGTH >> 1) - (HITBOX_WIDTH >> 1);
 
-simple_arm::simple_arm(fish_game& g, bn::rect bounds, bn::fixed move_speed, bn::fixed swipe_speed) : 
-    _g(g),
-    _spr(bn::sprite_items::fritozoomarm.create_sprite()),
-    _bounds(bounds),
-    _move_speed(move_speed),
-    _swipe_speed(swipe_speed),
-    _state(arm_state::MOVING) {
-    _spr.set_rotation_angle(BASE_ANGLE);
+simple_arm::simple_arm(fish_game &g, bn::rect bounds, bn::fixed move_speed, bn::fixed swipe_speed) : _g(g),
+                                                                                                     _elbow_spr(bn::sprite_items::fritozoomarm.create_sprite()),
+                                                                                                     _paw_spr(bn::sprite_items::fritozoomarm.create_sprite()),
+                                                                                                     _bounds(bounds),
+                                                                                                     _move_speed(move_speed),
+                                                                                                     _swipe_speed(swipe_speed),
+                                                                                                     _state(arm_state::MOVING)
+{
+    _paw_spr.set_rotation_angle(BASE_ANGLE);
+    _elbow_spr.set_rotation_angle(270);
 }
 
-void simple_arm::update() {
-    switch (_state) {
+void simple_arm::update()
+{
+    switch (_state)
+    {
     case arm_state::MOVING:
-        if(bn::keypad::a_pressed()) {
+        if (bn::keypad::a_pressed())
+        {
             _state = arm_state::SWIPING;
-        } else {
+        }
+        else
+        {
             _move();
         }
         break;
@@ -49,48 +55,60 @@ void simple_arm::update() {
     }
 }
 
-void simple_arm::set_state(arm_state state) {
+void simple_arm::set_state(arm_state state)
+{
     _state = state;
 }
 
-bn::optional<bn::rect> simple_arm::hitbox() {
-    if(_state != arm_state::SWIPING) return {};
+bn::optional<bn::rect> simple_arm::hitbox()
+{
+    if (_state != arm_state::SWIPING)
+        return {};
     // TODO determine whether negating the rotation is appropriate or if the degrees_to_arrow is backward
-    bn::fixed_point hit_center = _spr.position() + degrees_to_arrow(-_spr.rotation_angle(), PAW_LOC);
+    bn::fixed_point hit_center = _paw_spr.position() + degrees_to_arrow(-_paw_spr.rotation_angle(), PAW_LOC);
     return bn::rect(round_fixed_point(hit_center), {HITBOX_WIDTH, HITBOX_WIDTH});
 }
 
-void simple_arm::_move() {
-    bn::fixed_point target = _spr.position();
-    // TODO - extract to utility? 
-    target.set_x(target.x() + _move_speed * 
-        tribool(bn::keypad::left_held(), bn::keypad::right_held()));
-    
-    target.set_y(target.y() + _move_speed * 
-        tribool(bn::keypad::up_held(), bn::keypad::down_held()));
+void simple_arm::_move()
+{
+    bn::fixed_point target = _paw_spr.position();
+    // TODO - extract to utility?
+    target.set_x(target.x() + _move_speed *
+                                  tribool(bn::keypad::left_held(), bn::keypad::right_held()));
 
-    _spr.set_position(clamp_point(_bounds, target));
+    target.set_y(target.y() + _move_speed *
+                                  tribool(bn::keypad::up_held(), bn::keypad::down_held()));
+
+    _paw_spr.set_position(clamp_point(_bounds, target));
+    _elbow_spr.set_position(_paw_spr.position() + bn::fixed_point{0, -30});
 }
 
-void simple_arm::_swipe() {
-    bn::fixed current = _spr.rotation_angle();
+void simple_arm::_swipe()
+{
+    bn::fixed current = _paw_spr.rotation_angle();
     // TODO: handle overshoot where angle wraps
-    if(current < MAX_ANGLE) {
+    if (current < MAX_ANGLE)
+    {
         current += _swipe_speed;
-        _spr.set_rotation_angle(current);
-    } else {
+        _paw_spr.set_rotation_angle(current);
+    }
+    else
+    {
         _state = arm_state::RETURNING;
     }
 }
 
-void simple_arm::_return() {
-    bn::fixed current = _spr.rotation_angle();
-    if (current > BASE_ANGLE) {
+void simple_arm::_return()
+{
+    bn::fixed current = _paw_spr.rotation_angle();
+    if (current > BASE_ANGLE)
+    {
         // TODO: handle overshoot where angle wraps
-        _spr.set_rotation_angle(current - (_swipe_speed / 2));
-    } else {
-        _spr.set_rotation_angle(BASE_ANGLE);
+        _paw_spr.set_rotation_angle(current - (_swipe_speed / 2));
+    }
+    else
+    {
+        _paw_spr.set_rotation_angle(BASE_ANGLE);
         _state = arm_state::MOVING;
     }
 }
-
