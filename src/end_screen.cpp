ec6e25f7fd4a123b5000818df12a494c6c24203d
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-09-13 15:23:33 +0000

    fixing dad fish transparency

--- src/end_screen.cpp
+++ src/end_screen.cpp
@@ -17,7 +17,7 @@ end_screen::end_screen() :
         bn::sprite_items::fritojump.tiles_item(),
         0,1,2,3,4,5,6,7,8,9,10,11
     )),
-    _stack(bn::fixed_point{-50, 0}, {}) {
+    _stack(bn::fixed_point{-50, 30}, {}) {
 }
 
 GAME_TYPE end_screen::update() {
--- src/fish_stack.cpp
+++ src/fish_stack.cpp
@@ -13,7 +13,7 @@ fish_stack::fish_stack(bn::fixed_point base_pos, bn::vector<FISH_TYPE, MAX_FISH>
     for(int i = 0; i < caught.size(); i++) {
         auto fish = FISH_TABLE[caught[i]];
         _fishes.push_back(fish.dead.create_sprite(
-            base_pos  - bn::fixed_point{0, 5*i}
+            base_pos  - bn::fixed_point{0, 6*i}
         ));
     }
 }
